ba795d5c629d90e5649e89670877094bfd4dfbed
rmarcais <remi.marcais87@gmail.com>
2026-02-26 10:47:50 +0100

    Add 7-rotate.sh file

---
+++ linux_security/1x01_shell_ops/7-rotate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ -d $1 ]
+then
+    mkdir -p $1/backups/
+    for filename in $1/*.log; do
+	if [ "$(stat -c %s $filename)" -gt 1024 ]
+	then
+	    gzip $filename
+	    mv $filename.gz $1/backups
+	else
+	    echo "Skipping small file: ${filename##*/}"
+	fi
+    done
+else
+    exit 1
+fi
