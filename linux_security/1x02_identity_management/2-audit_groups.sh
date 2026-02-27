12a5ea4c053ab168bfe3ad76cb062356cf2b4aa0
rmarcais <remi.marcais87@gmail.com>
2026-02-27 10:20:11 +0100

    Add 2-audit_groups.sh file

---
+++ linux_security/1x02_identity_management/2-audit_groups.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+awk -F':' '{if ($3 >= 1000) print $1 }' /etc/passwd |
+while read -r user; do
+    id -nG $user 2>/dev/null |
+    tr ' ' '\n' |
+    grep -Ex '^(disk|docker|shadow)$' |
+    sed "s/^/$user:/"
+done
