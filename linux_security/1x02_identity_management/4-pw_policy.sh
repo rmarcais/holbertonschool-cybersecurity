c45cb88ebf444cf6fc79c2e91c7d8947109eed36
rmarcais <remi.marcais87@gmail.com>
2026-03-03 11:28:12 +0100

    Update script

--- linux_security/1x02_identity_management/4-pw_policy.sh
+++ linux_security/1x02_identity_management/4-pw_policy.sh
@@ -8,7 +8,7 @@ rule="password        requisite                       pam_pwquality.so minlen=12
 
 if grep -Eq '^[[:space:]]*password[[:space:]]+requisite[[:space:]]+pam_pwquality\.so' $file
 then
-    sed -ir 's|^[[:space:]]*password[[:space:]]+requisite[[:space:]]+pam_pwquality\.so.*$|"$rule"|' $file
+    sed -i -r 's|^[[:space:]]*password[[:space:]]+requisite[[:space:]]+pam_pwquality\.so.*$|"$rule"|' $file
 else
     tmp=$(mktemp)
     awk -v rule="$rule" '!done && $0 ~ /^[[:space:]]*password[[:space:]]+.*pam_unix\.so/ { print rule; done=1 } {print}' $file > $tmp
