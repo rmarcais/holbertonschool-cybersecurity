5048195c1a28686a929b6a8604a0e706abce0184
rmarcais <remi.marcais87@gmail.com>
2026-03-03 13:41:39 +0100

    Add 6-onboard.sh and 6-flag.txt files

---
+++ linux_security/1x02_identity_management/6-onboard.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+useradd $1
+
+passwd -l $1
+
+mkdir -p /home/$1/.ssh/
+chmod 700 /home/$1/.ssh/
+
+echo $2 >> /home/$1/.ssh/authorized_keys
+chmod 600 /home/$1/.ssh/authorized_keys
+
+chown -R $1:$1 /home/$1/.ssh/
+chown -R $1:$1 /home/$1/.ssh/authorized_keys
