597f953b1194997e65a1de0029b319707e861c7a
rmarcais <remi.marcais87@gmail.com>
2026-03-03 10:03:03 +0100

    Use set command

--- linux_security/1x02_identity_management/3-harden_ssh.sh
+++ linux_security/1x02_identity_management/3-harden_ssh.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+set -e
+
 sed -i -r 's/^(PermitRootLogin|PasswordAuthentication)([[:space:]]+)yes/\1\2no/' $1
 sed -i -r 's/^(PubkeyAuthentication)([[:space:]]+)no/\1\2yes/' $1
 
 sshd -t > /dev/null 2>&1
-if [ $? -eq 0 ]; then
-    /etc/init.d/ssh reload > /dev/null 2>&1
-fi
+/etc/init.d/ssh reload > /dev/null 2>&1
