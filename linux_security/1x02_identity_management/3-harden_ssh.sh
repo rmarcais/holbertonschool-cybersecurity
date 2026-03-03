fb52df7a71fb1b556b7e584b8a29d94ce057d5e4
rmarcais <remi.marcais87@gmail.com>
2026-03-03 10:09:57 +0100

    Update regex

--- linux_security/1x02_identity_management/3-harden_ssh.sh
+++ linux_security/1x02_identity_management/3-harden_ssh.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
-sed -i -r 's/^(PermitRootLogin|PasswordAuthentication)([[:space:]]+)yes/\1\2no/' $1
-sed -i -r 's/^(PubkeyAuthentication)([[:space:]]+)no/\1\2yes/' $1
+sed -i -r 's/^PermitRootLogin yes/PermitRootLogin no/' $1
+sed -i -r 's/^PasswordAuthentication yes/PasswordAuthentication no/' $1
+sed -i -r 's/^PubkeyAuthentication no/PubkeyAuthentication yes/' $1
 
 sshd -t > /dev/null 2>&1
 /etc/init.d/ssh reload > /dev/null 2>&1
