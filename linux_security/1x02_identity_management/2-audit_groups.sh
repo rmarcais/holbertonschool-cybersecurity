169fa53f8160b2d893b76ed040f32cae5de40a68
rmarcais <remi.marcais87@gmail.com>
2026-03-03 10:49:35 +0100

    Update scipt to use a for loop

--- linux_security/1x02_identity_management/2-audit_groups.sh
+++ linux_security/1x02_identity_management/2-audit_groups.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+
+groups=("disk" "docker" "shadow")
+
 awk -F: '{if ($3>=1000) print $1 }' $1 |
 while read -r user; do
-    id -nG $user 2>/dev/null |
-    tr ' ' '\n' |
-    grep -Ex '^(disk|docker|shadow)$' |
-    sed "s/^/$user:/"
+    for ug in $(id -nG $user 2>/dev/null); do
+	for g in ${groups[@]}; do
+	    if [ $ug = $g ]; then
+		echo "$user:$g"
+	    fi
+	done
+    done
 done
