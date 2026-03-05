77dc9805359ef06ff23766c0f7295738dfec7b09
rmarcais <remi.marcais87@gmail.com>
2026-03-05 16:03:23 +0100

    Add check_integrity function and 2-flag.txt

--- linux_security/1x04_security_automation/sentinel.sh
+++ linux_security/1x04_security_automation/sentinel.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 CONFIG_FILE="./sentinel.conf"
+BACKUPS="/var/backups/sentinel/"
 
 if [ ! -z "$CONFIG_FILE" ] && [ -f "$CONFIG_FILE" ]; then
     source "$CONFIG_FILE"
@@ -27,3 +28,25 @@ check_services() {
 }
 
 check_services
+
+check_integrity() {
+    for ftw in "${FILES_TO_WATCH[@]}"
+    do
+	GOLD_COPY="$BACKUPS${ftw##*/}.gold"
+	HASH_GOLD_COPY=$(md5sum "$GOLD_COPY" | awk '{print $1}')
+	HASH_FTW=$(md5sum "$ftw" | awk '{print $1}')
+
+	if [ "$HASH_GOLD_COPY" = "$HASH_FTW" ]; then
+	    echo "OK: $ftw integrity verified"
+	else
+	    if cp "$GOLD_COPY" "$ftw" > /dev/null; then
+                echo "FIXED: Restored $ftw"
+            else
+                echo "Error checking integrity of $ftw"
+            fi
+
+	fi
+    done
+}
+
+check_integrity
