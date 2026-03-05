bf60d3da4efe5e0b448f9d8499e02e5a8f58eb9e
rmarcais <remi.marcais87@gmail.com>
2026-03-05 15:04:05 +0100

    Add check_services fuction and 1-flag.txt file

--- linux_security/1x04_security_automation/sentinel.sh
+++ linux_security/1x04_security_automation/sentinel.sh
@@ -10,3 +10,20 @@ fi
 
 : "${SERVICES:?Variable SERVICES not defined}"
 : "${FILES_TO_WATCH:?Variable FILES_TO_WATCH not defined}"
+
+check_services() {
+    for svc in "${SERVICES[@]}"
+    do
+	if pgrep -f "$svc" > /dev/null; then
+	    echo "OK: $svc is running"
+	else
+	    if eval "$svc" > /dev/null; then
+	        echo "FIXED: Restarted $svc"
+	    else
+		echo "Error starting $svc"
+	    fi
+	fi
+    done
+}
+
+check_services
