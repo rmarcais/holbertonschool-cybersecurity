2d37643fa35b272cfc8388d82bcad9059adb840b
rmarcais <remi.marcais87@gmail.com>
2026-04-13 13:24:27 +0200

    Make the script one line long

--- linux_security/1x03_system_visibility/11-kernel.sh
+++ linux_security/1x03_system_visibility/11-kernel.sh
@@ -1,16 +1,2 @@
 #!/bin/bash
-
-FILE_1="/var/log/kern.log"
-FILE_2="/var/log/messages"
-
-if [ -f $FILE_1 ]; then
-    grep $1 $FILE_1
-else
-    echo "$FILE_1 doesn't exist"
-fi
-
-if [ -f $FILE_2 ]; then
-    grep $1 $FILE_2
-else
-    echo "$FILE_2 doesn't exist"
-fi
+grep -q $1 /var/log/kern.log /var/log/messages
