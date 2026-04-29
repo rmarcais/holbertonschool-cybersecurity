b03e0d6442b1cc77b56d89132824acb8c67cabd6
rmarcais <remi.marcais87@gmail.com>
2026-04-29 10:59:58 +0200

    Make the scrip 2 lines long

--- network_security/2x00_network_fundamentals/3-ip2bin.sh
+++ network_security/2x00_network_fundamentals/3-ip2bin.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-echo "$1" | awk -F'.' '{
-	for(i=1; i<=4; i++) {
-		cmd="echo \"obase=2;"$i"\" | bc"
-		cmd | getline binary
-		close(cmd)
-		printf "%08d%s", binary, (i<4?".":"\n")
-	}
-}'
+echo "$1" | awk -F'.' '{for(i=1; i<=4; i++) {cmd="echo \"obase=2;"$i"\" | bc";cmd | getline binary;close(cmd);printf "%08d%s", binary, (i<4?".":"\n")}}'
