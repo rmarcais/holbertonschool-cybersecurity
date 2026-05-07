867bf6c2e6d27b15ad026d0e090cbdeaab635760
rmarcais <remi.marcais87@gmail.com>
2026-05-07 15:11:27 +0200

    Use IFS in the script

--- network_security/2x00_network_fundamentals/5-network_id.sh
+++ network_security/2x00_network_fundamentals/5-network_id.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-read ip1 ip2 ip3 ip4 <<<"${1//./ }" ; read m1 m2 m3 m4 <<<"${2//./ }" ; printf "%d.%d.%d.%d" $((ip1 & m1)) $((ip2 & m2)) $((ip3 & m3)) $((ip4 & m4))
+IFS=. read ip1 ip2 ip3 ip4 <<<"$1" ; IFS=. read m1 m2 m3 m4 <<<"$2" ; printf "%d.%d.%d.%d" $((ip1 & m1)) $((ip2 & m2)) $((ip3 & m3)) $((ip4 & m4))
