80872a68a7a36085fb9b48320ca9adfc74b599af
rmarcais <remi.marcais87@gmail.com>
2026-06-05 15:50:38 +0200

    Edit 10-beacon.sh

--- network_security/2x03_traffic_forensics/10-beacon.sh
+++ network_security/2x03_traffic_forensics/10-beacon.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tshark -r "$1" -Y 'ip.addr == 10.10.10.50 && ip.addr == 172.16.42.66' -T fields -e frame.time_epoch
+tshark -r "$1" -Y 'ip.addr == 10.10.10.50 && ip.addr == 172.16.42.66' -T fields -e frame.time
